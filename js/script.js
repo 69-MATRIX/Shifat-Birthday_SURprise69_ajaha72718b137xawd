9e6b4a0df305a8ee46b6eec40dfdea0f9fccfa9a
69-MATRIX <muntasinislam98@gmail.com>
2026-07-22 20:29:58 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+// ==========================
+// Preload Images
+// ==========================
+
+const images = [
+    "photos/photo1.jpg",
+    "photos/photo2.jpg",
+    "photos/photo3.jpg",
+    "photos/photo4.jpg",
+    "photos/photo5.jpg",
+    "photos/photo6.jpg",
+    "photos/photo7.jpg",
+    "photos/photo8.jpg",
+    "photos/photo9.jpg"
+];
+
+images.forEach(src => {
+    const img = new Image();
+    img.src = src;
+});
 // ------------------------
 // Loading Screen
 // ------------------------
